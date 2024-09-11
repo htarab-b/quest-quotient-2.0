d5755b3c1bd51a1b2942fc8a6d74adf499d7a78d
Barath B <barath@Baraths-MacBook-Air.local>
2024-09-12 00:05:15 +0530

    Disabled scroll for first 5 seconds upon opening

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {motion} from 'framer-motion';
 
 import down_icon from '../assets/down-icon.png'
 
 function Landing() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center flex-wrap" id='Landing'>
-        <div className="text-center w-full pointer-events-none">
-            <motion.h1
+    useEffect(() => {
+        // Disable scrolling
+        document.body.style.overflow = 'hidden';
+
+        // Enable scrolling after 2 seconds
+        const timer = setTimeout(() => {
+        document.body.style.overflow = 'auto';
+        }, 4000);
+
+        // Cleanup timer if component unmounts
+        return () => clearTimeout(timer);
+    }, []);
+    return (
+        <div className="min-h-screen bg-black text-white flex items-center justify-center flex-wrap" id='Landing'>
+            <div className="text-center w-full pointer-events-none">
+                <motion.h1
+                initial = {{ opacity:0 }}
+                animate = {{ opacity:1 }}
+                transition = {{ duration:2, delay:0.75}}
+                className="LogoFont sm:text-6xl text-4xl">
+                    Quest Quotient
+                </motion.h1>
+                <motion.h2
+                initial = {{ opacity:0 }}
+                animate = {{ opacity:1 }}
+                transition = {{ duration:2, delay:1.5}}
+                className="LogoFont sm:pt-4 pt-2 sm:text-2xl">2.0</motion.h2>
+            </div>
+            <motion.div
             initial = {{ opacity:0 }}
-            animate = {{ opacity:1 }}
-            transition = {{ duration:2, delay:0.75}}
-            className="LogoFont sm:text-6xl text-4xl">
-                Quest Quotient
-            </motion.h1>
-            <motion.h2
-            initial = {{ opacity:0 }}
-            animate = {{ opacity:1 }}
-            transition = {{ duration:2, delay:1.5}}
-            className="LogoFont sm:pt-4 pt-2 sm:text-2xl">2.0</motion.h2>
+            animate = {{ opacity:0.7 }}
+            transition = {{ duration:2.5, delay:3}}
+            onScroll = {{opacity:0}}
+            className="absolute bottom-4 pointer-events-none flex justify-center items-center flex-wrap sm:w-40 w-32">
+                <h2 className="sm:text-2xl text-xl text-center">
+                    Scroll Down
+                </h2>
+                <img src={down_icon} className="sm:w-10 w-8" />
+            </motion.div>
         </div>
-        <motion.div
-        initial = {{ opacity:0 }}
-        animate = {{ opacity:0.7 }}
-        transition = {{ duration:2.5, delay:3}}
-        onScroll = {{opacity:0}}
-        className="absolute bottom-4 pointer-events-none flex justify-center items-center flex-wrap sm:w-40 w-32">
-            <h2 className="sm:text-2xl text-xl text-center">
-                Scroll Down
-            </h2>
-            <img src={down_icon} className="sm:w-10 w-8" />
-        </motion.div>
-    </div>
-  )
+    )
 }
 
 export default Landing
