718e1ef0f465a03ff4d0d5cd0b2f9259ed22cade
Barath B <barath@Baraths-MacBook-Air.local>
2024-09-24 22:46:09 +0530

    Closed registrations for all events

--- src/components/events/Buildathon.jsx
+++ src/components/events/Buildathon.jsx
@@ -17,112 +17,12 @@ function Buildathon() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-black text-white">
             <section className="py-6">
-                <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+                <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8">
                     <div className="py-6 md:py-0 md:px-6 my-auto">
-                        <h1 className="text-4xl font-bold text-center">Buildathon Registration</h1>
+                        <h1 className="text-4xl font-bold text-center">Buildathon</h1>
                         <h1 className="text-2xl text-center">(25-09-2024)</h1>
-                        <p className="pt-2 pb-4 text-center">Please read the rulebook below before applying</p>
-                        <div className="">
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Team must consist of atleast of 1 and maximum of 5 members and should be registered before the given deadline.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Project must be related to the given theme for buildathon.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Teams must present their projects within their allocated time (Explaining the concept, design and functionality.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Projects will be evaluated on innovation, functionality, design and project related to the theme.</span>
-                            </p>
-                        </div>
+                        <p className="pt-2 pb-4 text-center">The registrations are closed for this event.</p>
                     </div>
-                    <form
-                        action="https://docs.google.com/forms/d/e/1FAIpQLSdzAiA1GfKv_L3uIHCk1OkAF9qaGc0WrCxi9IxY7PLPdB-xLg/formResponse"
-                        method="post"
-                        target="hidden_iframe"
-                        onSubmit={() => setSubmitted(true)}
-                        className="flex flex-col py-6 space-y-6 md:py-0 px-6 my-auto">
-
-                        <input type="hidden" name="entry.1247644955" value="Buildathon" />
-
-                        <h3 className="text-2xl font-bold text-center">Fill the form to register</h3>
-
-                        {/* Dropdown to select the number of participants */}
-                        <label className="block">
-                            <span className="mb-1">Number of Participants</span>
-                            <select
-                                value={numParticipants}
-                                onChange={changeNumParticipants}
-                                className="block w-full rounded-sm shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-600 text-black p-1"
-                                required
-                                disabled={submitted}
-                            >
-                                {Array.from({ length: maxParticipants - minParticipants + 1 }, (_, i) => (
-                                    <option key={i} value={minParticipants + i}>
-                                        {minParticipants + i}
-                                    </option>
-                                ))}
-                            </select>
-                        </label>
-
-                        <label className="block my-2">
-                            <span className="mb-1">College Name</span>
-                            <input
-                                type="text"
-                                name="entry.919534871"
-                                className="block w-full rounded-sm shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-600 text-black p-1"
-                                required
-                                readOnly={submitted}
-                            />
-                        </label>
-
-                        {/* Dynamically rendered participant input fields */}
-                        {Array.from({ length: numParticipants }, (_, index) => (
-                            <div key={index}>
-                                <label className="block my-2">
-                                    <span className="mb-1">Participant {index + 1} Name</span>
-                                    <input
-                                        type="text"
-                                        name={participantentryname[index]}
-                                        className="block w-full rounded-sm shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-600 text-black p-1"
-                                        required
-                                        readOnly={submitted}
-                                    />
-                                </label>
-                                <label className="block my-2">
-                                    <span className="mb-1">Participant {index + 1} Phone Number</span>
-                                    <input
-                                        type="text"
-                                        name={participantentryphone[index]}
-                                        className="block w-full rounded-sm shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-600 text-black p-1"
-                                        required
-                                        readOnly={submitted}
-                                    />
-                                </label>
-                            </div>
-                        ))}
-
-                        <button
-                            type="submit"
-                            className="self-center px-5 py-1 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 bg-violet-600 text-gray-50 focus:ring-violet-600 hover:ring-violet-600"
-                            hidden={submitted}
-                        >
-                            Submit
-                        </button>
-                    </form>
                 </div>
             </section>
             <iframe
--- src/components/events/Hackathon.jsx
+++ src/components/events/Hackathon.jsx
@@ -17,112 +17,12 @@ function Hackathon() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-black text-white">
             <section className="py-6">
-                <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+                <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8">
                     <div className="py-6 md:py-0 md:px-6 my-auto">
                         <h1 className="text-4xl font-bold text-center">Hackathon Registration</h1>
                         <h1 className="text-2xl text-center">(25-09-2024)</h1>
-                        <p className="pt-2 pb-4 text-center">Please read the rulebook below before applying</p>
-                        <div className="">
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Teams should consist of exactly 3 members.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Teams should register before the given deadline.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Projects must be original. No reuse of previous projects.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Each team will be given 5 minutes to present their project.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Plagiarism or outsourcing will lead to disqualification.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">The judges' decision is final and binding.</span>
-                            </p>
-                            <p className="flex items-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="w-1/12 h-1/12 mr-1">
-                                    <path d="M12.096 16q0 1.632 1.152 2.784t2.752 1.12 2.752-1.12 1.152-2.784-1.152-2.752-2.752-1.152-2.752 1.152-1.152 2.752z"></path>
-                                </svg>
-                                <span className="w-11/12 mt-1 sm:mt-2">Every Participant has to bring their own devices.</span>
-                            </p>
-                        </div>
+                        <p className="pt-2 pb-4 text-center">The registrations are closed for this event.</p>
                     </div>
-                    <form
-                        action="https://docs.google.com/forms/d/e/1FAIpQLSdzAiA1GfKv_L3uIHCk1OkAF9qaGc0WrCxi9IxY7PLPdB-xLg/formResponse"
-                        method="post"
-                        target="hidden_iframe"
-                        onSubmit={() => setSubmitted(true)}
-                        className="flex flex-col py-6 space-y-6 md:py-0 px-6 my-auto">
-
-                        <input type="hidden" name="entry.1247644955" value="Hackathon" />
-
-                        <h3 className="text-2xl font-bold text-center">Fill the form to register</h3>
-
-                        <label className="block my-2">
-                            <span className="mb-1">College Name</span>
-                            <input
-                                type="text"
-                                name="entry.919534871"
-                                className="block w-full rounded-sm shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-600 text-black p-1"
-                                required
-                                readOnly={submitted}
-                            />
-                        </label>
-
-                        {/* Dynamically rendered participant input fields */}
-                        {Array.from({ length: numParticipants }, (_, index) => (
-                            <div key={index}>
-                                <label className="block my-2">
-                                    <span className="mb-1">Participant {index + 1} Name</span>
-                                    <input
-                                        type="text"
-                                        name={participantentryname[index]}
-                                        className="block w-full rounded-sm shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-600 text-black p-1"
-                                        required
-                                        readOnly={submitted}
-                                    />
-                                </label>
-                                <label className="block my-2">
-                                    <span className="mb-1">Participant {index + 1} Phone Number</span>
-                                    <input
-                                        type="text"
-                                        name={participantentryphone[index]}
-                                        className="block w-full rounded-sm shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-600 text-black p-1"
-                                        required
-                                        readOnly={submitted}
-                                    />
-                                </label>
-                            </div>
-                        ))}
-
-                        <button
-                            type="submit"
-                            className="self-center px-5 py-1 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 bg-violet-600 text-gray-50 focus:ring-violet-600 hover:ring-violet-600"
-                            hidden={submitted}
-                        >
-                            Submit
-                        </button>
-                    </form>
                 </div>
             </section>
             <iframe
